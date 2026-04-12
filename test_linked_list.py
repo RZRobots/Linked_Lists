a740d4e6fe780acf6c7a4d19a88a7a3cb011cbed
RZicman <zicmanjo@gmail.com>
2026-04-12 14:22:15 +0100

    ADD tests for isEmpty

--- test_linked_list.py
+++ test_linked_list.py
@@ -2,5 +2,19 @@
 
 from Linked_List import Linked_List, Node
 
+def test_isEmpty_true():
+    obj = Linked_List()
+
+    assert obj.isEmpty() == True
+    
+def test_isEmpty_false():
+    obj = Linked_List()
+    node = Node(7)
+
+    obj.head = node
+    obj.size = 1
+
+    assert obj.isEmpty() == False
+
 
 
